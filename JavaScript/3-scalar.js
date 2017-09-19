6209de8b24d085b916162b917ae40ff1491114d6
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2017-09-19 21:01:29 +0300

    Small update
    Updated numbers in 3-scalar.js
    Fixed name of variable in 5...js

--- JavaScript/3-scalar.js
+++ JavaScript/3-scalar.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const scalar1 = 5;
+const scalar1 = 100;
 const scalar2 = scalar1;
 
 const object1 = { field: scalar1 };
@@ -10,7 +10,7 @@ console.dir({ object1 });
 console.dir({ object2 });
 console.dir({ scalar1 });
 
-object1.field = 6;
+object1.field = 600;
 
 console.dir({ object1 });
 console.dir({ object2 });
